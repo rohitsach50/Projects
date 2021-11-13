4f9a3621cf3db868befda50c0e526169a124aa9f
rohit kumar <rohitsach50@gmail.com>
2021-11-13 13:21:44 +0530

    Create data_extracting.py
    This file downloads file from data.gov.in and moved to aws s3 using cron.

---
+++ ETL Pipeline/AirQuality/data_extracting.py
@@ -0,0 +1,25 @@
+import time
+import os
+import requests
+
+
+# url = 'https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?format=csv&limit=all&api-key=579b464db66ec23bdd000001cdc3b564546246a772a26393094f5645'
+url = 'https://api.data.gov.in/resource/3b01bcb8-0b14-4abf-b6f2-c1bfd384ba69?api-key=579b464db66ec23bdd000001cdc3b564546246a772a26393094f5645&format=csv&offset=0&limit=all'
+myfile = requests.get(url)
+
+open('dex.csv', 'wb').write(myfile.content)
+
+
+f_path = 'dex.csv'
+
+t = os.path.getctime(f_path)
+
+t_str = time.ctime(t)
+
+t_obj = time.strptime(t_str)
+
+form_t = time.strftime("%Y-%m-%d %H:%M:%S", t_obj)
+
+form_t = form_t.replace(':', '˸') #Colon replaced with special unicode character U+02F8
+
+os.rename(f_path, os.path.split(f_path)[0] + '' + form_t + os.path.splitext(f_path)[1])
